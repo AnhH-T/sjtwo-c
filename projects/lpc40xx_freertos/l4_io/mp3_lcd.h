fbf54758322523953d7241d0d747780788233dd2
vthanhnguyen <vthanhnguyen95@yahoo.com>
2021-11-18 13:35:43 -0800

    slave state machine

--- projects/lpc40xx_freertos/l4_io/mp3_lcd.h
+++ projects/lpc40xx_freertos/l4_io/mp3_lcd.h
@@ -23,26 +23,41 @@
 #define read_bit 1
 #define write_bit 0
 
-#define LCD_INCREMENT 1
-#define LCD_DECREMENT 0
-#define LCD_SHIFT 1
-#define LCD_NO_SHIFT 0
-#define LCD_DISPLAY_ON 1
-#define LCD_DISPLAY_OFF 0
-#define LCD_CURSOR_ON 1
-#define LCD_CURSOR_OFF 0
-#define LCD_CURSOR_BLINK_ON 1
-#define LCD_CURSOR_BLINK_OFF 0
-#define LCD_MOVE_CURSOR 1
-#define LCD_MOVE_SHIFT 0
-#define LCD_MOVE_RIGHT 1
-#define LCD_MOVE_LEFT 0
-#define LCD_LENGTH_8BIT 1
-#define LCD_LENGTH_4BIT 0
-#define LCD_2_LINE 1
-#define LCD_1_LINE 0
-#define LCD_FONT_5_10 1
-#define LCD_FONT_5_8 0
+// flags for display entry mode
+#define LCD_ENTRYRIGHT 0x00
+#define LCD_ENTRYLEFT 0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
+
+// flags for display on/off control
+#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYOFF 0x00
+#define LCD_CURSORON 0x02
+#define LCD_CURSOROFF 0x00
+#define LCD_BLINKON 0x01
+#define LCD_BLINKOFF 0x00
+
+// flags for display/cursor shift
+#define LCD_DISPLAYMOVE 0x08
+#define LCD_CURSORMOVE 0x00
+#define LCD_MOVERIGHT 0x04
+#define LCD_MOVELEFT 0x00
+
+// flags for function set
+#define LCD_8BITMODE 0x10
+#define LCD_4BITMODE 0x00
+#define LCD_2LINE 0x08
+#define LCD_1LINE 0x00
+#define LCD_5x10DOTS 0x04
+#define LCD_5x8DOTS 0x00
+
+// flags for backlight control
+#define LCD_BACKLIGHT 0x08
+#define LCD_NOBACKLIGHT 0x00
+
+#define En B00000100  // Enable bit
+#define Rw B00000010  // Read/Write bit
+#define Rs B00000001  // Register select bit
 
 typedef struct {
   uint8_t LCD_SCL;
